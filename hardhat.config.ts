41fcf93251bccfcffb5118b53b4086e892f6df67
VitalR <vitalii.rodikov@gmail.com>
2023-02-08 14:34:05 +0200

    Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17"
+      },
+      {
+        version: "0.6.0"
+      },
+      {
+        version: "0.5.0"
+      }
+    ]
+  },
   networks: {
     goerli: {
       url: process.env.GOERLI_URL || "",
